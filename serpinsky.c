73dcb466fa2f54005f6bcd3d76e0328888b8c7b8
Лагодюк Юрий <cafebabe@yandex-team.com.ua>
2013-03-25 03:31:00 +0200

    Building sierpinski triangle with IFS

--- serpinsky.c
+++ serpinsky.c
@@ -1,11 +1,43 @@
+#include <stdlib.h>
 #include "bmp.h"
 
 int main() {
-	Canvas * canv = new_canvas(115, 20);
-	set_pixel(10, 10, rgb(255, 0, 0), canv);
-	set_pixel(11, 10, rgb(0, 255, 0), canv);
-	set_pixel(12, 10, rgb(0, 0, 255), canv);
-	set_pixel(0, 0, rgb(255, 255, 255), canv);
-	write_bmp("test.bmp", canv);
+	int x0 = 1;
+	int y0 = 1;
+	
+	int x1 = 199;
+	int y1 = 1;
+
+	int x2 = 1;
+	int y2 = 199;
+
+	int x = 10;
+	int y = 10;
+	
+	Canvas * canv = new_canvas(200, 200);
+
+	int i;
+	int r;
+
+	for(i = 0; i < 10000; i++) {
+		set_pixel(x, y, rgb(255, 255, 0), canv);
+		r = rand() % 3;
+		switch(r) {
+			case 0:
+				x = (x0 + x) / 2;
+				y = (y0 + y) / 2;
+				break;
+			case 1:
+				x = (x1 + x) / 2;
+                                y = (y1 + y) / 2;
+				break;
+			case 2:
+				x = (x2 + x) / 2;
+                                y = (y2 + y) / 2;
+				break;
+		}
+	}
+
+	write_bmp("fract.bmp", canv);
 	return 0;
 }
